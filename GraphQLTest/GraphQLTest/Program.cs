2baa5a756cad9af94a888f365e803f8ca87ba268
Elvar Þór Ólafsson <b0b4f3tt@gmail.com>
2019-10-19 21:36:49 +0000

    Most likely final version
    Completed the walkthrough on the site. Added all the necessary classes for this demo to work.

--- GraphQLTest/GraphQLTest/Program.cs
+++ GraphQLTest/GraphQLTest/Program.cs
@@ -11,22 +11,29 @@ namespace GraphQLTest
         {
             Console.WriteLine("Hello World!");
             var schema = Schema.For(@"
-                    type Query {
-                        hello: String,
-                        jedis: [Jedi]
-                    }
-
-                    type Jedi {
-                        name: String,
-                        side: String
-                    }
-            ");
+                type Jedi {
+                    name: String,
+                    side: String,
+                    id: ID
+                }                    
+                type Query {
+                    hello: String,
+                    jedis: [Jedi],
+                    jedi(id: ID): Jedi
+                }                    
+            ", _ =>
+            {
+                _.Types.Include<Query>();
+            });
 
             var root = new { Hello = "Hello World" };
             var json = schema.Execute(_ =>
             {
-                _.Query = "{ hello}";
-                _.Root = root;
+                //_.Query = "{ jedis { name, side}}";
+                _.Query = "{ jedi(id: 1) { name}}";
+                //_.Query = "{ hello}";
+
+                //_.Root = root;
             });
 
             Console.WriteLine(json);
